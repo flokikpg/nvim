c89af9e42df15a74a842c67558d31d680d620f6c
sherpapraveen <sherpapraveen@gmail.com>
2026-01-02 15:20:33 +0530

    Update

---
+++ nvim/lua/plugins/lsp/mason.lua
@@ -0,0 +1,35 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+   opts = {
+     -- list of servers for mason to install
+     ensure_installed = {
+       "ts_ls",
+       "html",
+       "cssls",
+       "tailwindcss",
+       "svelte",
+       "lua_ls",
+       "graphql",
+       "emmet_ls",
+       "prismals",
+       "pyright",
+       "eslint",
+     },
+  },
+  dependencies = {
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      },
+    },
+    "neovim/nvim-lspconfig",
+  }
+}
+
